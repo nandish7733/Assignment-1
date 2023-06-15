58f953759e9b0e9193e8f19b1fd081e9c7b585bd
nandish7733 <136687114+nandish7733@users.noreply.github.com>
2023-06-15 13:13:17 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,7 @@
+
+n1=int(input("Enter marks of subject 1: "))
+n2=int(input("Enter marks of subject 2: "))
+n3=int(input("Enter marks of subject 3: ")) 
+sum=n1+n2+n3
+avg=sum/3
+print("The average of n1,n2 and n3 is",avg)
